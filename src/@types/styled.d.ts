9997874c69530fc61a421977e82a67dea96d3564
danilocucharro <danicucharro@outlook.com>
2024-02-20 09:50:51 -0300

    configuradas as rotas da aplicação

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,9 @@
-import 'styled-components';
+import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
-declare module 'styled-components' {//criando uma tipaem para o modulo do styled-components
+declare module 'styled-components' {
+  // criando uma tipaem para o modulo do styled-components
   export interface DefaultTheme extends ThemeType {}
 }
